3912c270a203bf277c5f2c61c2f4a2d574241abc
akishoreS <aryasingh0127@gmail.com>
2024-08-03 11:55:47 +0530

    latest changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "storage.googleapis.com",
+            "dl.airtable.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "storage.googleapis.com",
+            "dl.airtable.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- src/api/opportunity/controllers/opportunity.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * opportunity controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::opportunity.opportunity');
