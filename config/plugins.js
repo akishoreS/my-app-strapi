efa1e6fd1589b3a27d953a2357e6181ddb5d890c
akishoreS <aryasingh0127@gmail.com>
2024-08-03 12:17:17 +0530

    latest changes

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,1 @@
-const fs = require('fs');
-
-module.exports = ({ env }) => ({
-  upload: {
-    provider: 'google-cloud-storage',
-    providerOptions: {
-      bucketName: env('GCS_BUCKET_NAME'),
-      publicFiles: true,
-      uniform: false,
-      serviceAccount: JSON.parse(fs.readFileSync(env('GCS_SERVICE_ACCOUNT_PATH'), 'utf8')),
-    },
-  },
-});
-
-
-  
+module.exports = () => ({});
